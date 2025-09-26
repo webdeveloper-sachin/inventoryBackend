88f0f5643d5a805002815b45df26f78ba6e8bc68
Sachin <99788718+webdeveloper-sachin@users.noreply.github.com>
2025-09-26 15:27:36 +0530

    shiment get by tracking id params added

--- routes/manifest.routes.js
+++ routes/manifest.routes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 router.route("/get").post(getManifest);
 router.route("/save").post(saveManifest);
 router.route("/all-manifest").get(getAllManifest);
+router.route("/").get(getManifestByShipment);
+
 
 module.exports = router;
